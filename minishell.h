2bd3f28e47a86fd7be05d5693542f93ab07d1d17
Victor Naslund Cuesta <vnaslund@c3r6s6.42malaga.com>
2023-11-22 14:54:36 +0100

    Basic get path function for execve

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vnaslund <vnaslund@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:21:53 by vnaslund          #+#    #+#             */
-/*   Updated: 2023/11/22 14:24:55 by vnaslund         ###   ########.fr       */
+/*   Updated: 2023/11/22 14:52:24 by vnaslund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,21 @@
 
 # include "libft/libft.h"
 # include <signal.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+// Path and ENV utils
+char	*get_path(char *cmd, char **env); // first parameter is the command without flags
+
+// Memory handling
+void	ft_free_array(void **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free (array);
+}
 
 #endif
