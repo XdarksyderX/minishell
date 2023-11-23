953aca9dcc038627b3c754e0be29e6322c7358b0
Victor Naslund Cuesta <vnaslund@c3r7s4.42malaga.com>
2023-11-23 18:58:09 +0100

    builtins added

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:21:53 by vnaslund          #+#    #+#             */
-/*   Updated: 2023/11/23 13:45:11 by vnaslund         ###   ########.fr       */
+/*   Updated: 2023/11/23 18:56:58 by vnaslund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@
 
 // builtins
 int		ft_echo(char **cmd); // Single and multiple -n handled, "" and '' parsing not taken into account
+int		ft_exit(char **cmd); // Copies bash functinality except when a number outside LONG range is entered
+int		ft_pwd(char **cmd);
+
 
 // Path and ENV utils
 char	*get_path(char *cmd, char **env); // first parameter is the command without flags
