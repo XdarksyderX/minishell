d6e946c4aa9e365929fc8bcec191324cb3254061
Victor Naslund Cuesta <vnaslund@c3r3s6.42malaga.com>
2023-11-25 16:57:30 +0100

    42 header

---
+++ src/helpers/mem.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mem.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 16:35:31 by vnaslund          #+#    #+#             */
+/*   Updated: 2023/11/25 16:48:08 by vnaslund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+extern t_shell	g_shell;
+
+void	ft_free_array(void **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free (array);
+	array = NULL;
+}
+
+void	ft_free_g_shell(void)
+{
+	ft_free_array((void **)g_shell.env);
+	ft_free_array((void **)g_shell.path);
+}
+
